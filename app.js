cb30bca11450a3a301bf93133c4d91f8815f86ee
Thanapong <thanapongsj1996@gmail.com>
2019-06-12 06:47:33 +0700

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+// app
+const app = express()
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log('Database connected'))
+
+// routes
 app.get('/', (req, res) => {
     res.send('hello form NodeJS')
 })
 
 const port = process.env.PORT
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
