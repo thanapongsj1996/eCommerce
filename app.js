01f6e233944a850516e28cb1ebd1121f71524c1d
Thanapong <thanapongsj1996@gmail.com>
2019-06-13 01:17:56 +0700

    API Signup

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 // import routes
@@ -13,6 +16,11 @@ mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => console.log('Database connected'))
+
+// middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 // routes middleware
 app.use('/api',userRoutes)
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const crypto = require('crypto')
-const uudiv1 = require('uudi/v1')
+const uuidv1 = require('uuid/v1')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -34,21 +34,22 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true })
 
+// virtual field
 userSchema.virtual('password')
-.set(function(password){
-    this._password = password
-    this.salt = uudiv1()
-    this.hashed_password = this.encryptPassword(password)
-})
-.get(function(){
-    return this.hashed_password
-})
+    .set(function (password) {
+        this._password = password
+        this.salt = uuidv1()
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(function () {
+        return this._password
+    })
 
-userSchema.method = {
-    encryptPassword: function(password){
-        if(!password) return ''
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return ''
         try {
-            return crypto.createHmac('sha1', thus.salt).update(password).digest('hex')
+            return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
         } catch (err) {
             return ''
         }
