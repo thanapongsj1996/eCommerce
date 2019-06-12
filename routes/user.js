1770c96a06ae8963c38a11b6fdd0301ec8a143ac
Thanapong <thanapongsj1996@gmail.com>
2019-06-12 18:46:46 +0700

    User Schema

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello from Nodejs')
-})
+const { sayHi } = require('../controllers/user')
+
+router.get('/', sayHi)
 
 module.exports = router
