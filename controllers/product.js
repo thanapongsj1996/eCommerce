a2cb49f085d3bb3258dabdefa792b97f12ec4e05
Thanapong <thanapongsj1996@gmail.com>
2019-06-13 21:05:48 +0700

    Create product validation

--- controllers/product.js
+++ controllers/product.js
@@ -14,10 +14,26 @@ exports.create = (req, res) => {
             })
         }
         let product = new Product(fields)
+
+        // check for all fields
+        const { name, description, price, category, quantity, shipping } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            })
+        }
+
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1MB in size'
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
+
         product.save((err, result) => {
             if (err) {
                 return res.status(400).json({
