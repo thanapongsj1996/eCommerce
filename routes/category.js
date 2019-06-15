83f133292d4999924bcde0b69dcde4b6e483435e
Thanapong <thanapongsj1996@gmail.com>
2019-06-15 19:46:11 +0700

    Change url name category->categories

--- routes/category.js
+++ routes/category.js
@@ -9,7 +9,7 @@ router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create)
 router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update)
 router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove)
-router.get('/category', list)
+router.get('/categories', list)
 
 router.param('userId', userById)
 router.param('categoryId', categoryById)
